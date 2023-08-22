79d9aae3b4fb8fb30f1ad69cfb75b5ac887e530d
JjungminLee <julie0964@naver.com>
2023-08-22 21:24:38 +0900

    init commit

---
+++ src/main/java/com/youT/seenEar/SeenEarApplication.java
@@ -0,0 +1,13 @@
+package com.youT.seenEar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeenEarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeenEarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/youT/seenEar/SeenEarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.youT.seenEar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeenEarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
